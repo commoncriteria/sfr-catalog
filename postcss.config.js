625c6ba93d383cfe1a08ddf6dc40c17c96fbf446
Livi Thomas <othomas@mitre.org>
2023-09-20 16:58:30 -0600

    Added in basic template for the Catalog. Still need to update tabs further.

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+import tailwindcss from "tailwindcss";
+
+import autoprefixer from "autoprefixer";
+// eslint-disable-next-line no-undef
+module.exports = {
+    plugins: [tailwindcss, autoprefixer],
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <div>
+              <nav className="navbar flex fixed w-screen bg-neutral text-neutral-content sticky top-0 z-30">
+                  <div className="font-title font-bold inline-flex text-lg md:text-3xl ml-3 pr-20 text-accent">Catalog</div>
+
+                  <div className="flex-1 flex justify-end mr-10">
+                      <form className="max-w-sm px-4">
+                          <div className="relative">
+                              <button>
+                                  <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                                      fill="none"
+                                      viewBox="0 0 24 24"
+                                      stroke="currentColor"
+                                  >
+                                      <path
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                          strokeWidth={2}
+                                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                      />
+                                  </svg>
+                              </button>
+                              <input
+                                  type="text"
+                                  placeholder="Search"
+                                  className="w-full py-3 pl-12 pr-4 input input-bordered input-accent rounded-full"
+                              />
+                          </div>
+                      </form>
+                  </div>
+              </nav>
+          </div>
+          {/* Tabs need updated, try out: https://tailwind-elements.com/docs/react/navigation/tabs/*/}
+          <div className="tabs flex-1 flex mt-3 ml-3">
+              <a className="tab tab-lg tab-lifted tab-active">Threat</a>
+              <a className="tab tab-lg tab-lifted">Objective</a>
+              <a className="tab tab-lg tab-lifted">SFR</a>
+              <a className="tab tab-lg tab-lifted">PP</a>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
