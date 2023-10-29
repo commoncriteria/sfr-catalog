c1a02e640fff06707d95102fc2b8aa83f156baff
Sean Choudhury <schoudhury@mitre.org>
2023-10-29 18:37:42 -0400

    add copy to clipboard for sfrs

--- src/Components/AccordionContent.jsx
+++ src/Components/AccordionContent.jsx
@@ -5,6 +5,9 @@ import * as query from "../utils/query.js";
 import SFRDatabase from "../assets/NIAPDocumentBundle.json";
 import XMLViewer from "react-xml-viewer";
 import Modal from "./Modal.jsx";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 /**
  * The Accordion class that displays the accordion
@@ -170,6 +173,23 @@ function AccordionContent(props) {
         }
     }
 
+    /**
+    * Isolating this function to be used with clipboard
+    */
+    const copySFRToClipboard = () => {
+        let sfr_content = null;
+        try {
+            sfr_content = query.getSfrContent(SFRDatabase, props.selectedSfrs[0])[0][props.ppName]["XML"];
+        } catch (e) {
+            console.log(e);
+        }
+
+        if (sfr_content) {
+            navigator.clipboard.writeText(sfr_content);
+            toast.success("Copied to Clipboard");
+        }
+    }
+
     // Return Function
     return (
         <div>
@@ -207,7 +227,7 @@ function AccordionContent(props) {
                 </AccordionHeader>
                 <AccordionBody className={"px-4 bg-gray-200"}>
                     <div className={props.type == "SFRs" ? "flex flex-col" : "flex flex-col h-64 overflow-auto"}>
-                        <div className="">
+                        <div>
                             <Stack direction="row" component="label" alignItems="center" justifyContent="center">
                                 <Typography>String</Typography>
                                 <AccentSwitch checked={props.toggle} inputProps={{ 'aria-label': 'controlled' }} size="medium"
@@ -215,7 +235,14 @@ function AccordionContent(props) {
                                 <Typography>XML</Typography>
                             </Stack>
                         </div>
-                        <div style={{ display:"flex", justifyContent:"center", alignContent: "center", marginTop:"10px", marginBottom:"10px" }}>
+                        <div className="flex justify-end"
+                            onClick={copySFRToClipboard}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+                            </svg>
+                        </div>
+                        <div style={{ display: "flex", justifyContent: "center", alignContent: "center", marginTop: "10px", marginBottom: "10px" }}>
                             {
                                 (props.type == "SFRs" && props.tds.length != 0) ?
                                     <Modal
@@ -233,6 +260,18 @@ function AccordionContent(props) {
                     </div>
                 </AccordionBody>
             </Accordion>
+            <ToastContainer
+                position="bottom-center"
+                autoClose={500}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
         </div>
     );
 }
